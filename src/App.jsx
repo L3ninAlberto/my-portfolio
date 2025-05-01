fa79f64038cce2edc0ad00ac7b38444ec1e7043d
Lenin Alberto <lenbertogomez00@gmail.com>
2025-05-01 16:03:42 -0700

    development of landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 function App() {
-  return <div className="text-blue-500 font-bold">hellow world!</div>;
+  return (
+    <div className="hp flex flex-col gap-y-16">
+      <div className="flex items-center justify-center gap-6 py-5 mt-16 border-2 border-primary shadow-box">
+        <div className="w-40 h-40">
+          <img
+            src="imgs/face.webp"
+            alt="Face"
+            className="w-full h-full rounded-full object-cover"
+          />
+        </div>
+        <div>
+          <h1 className="text-4xl font-bold text-primary">
+            Lenin Alberto Gómez Durán
+          </h1>
+          <p className="text-2xl font-bold text-secondary">
+            Ingeniero en Desarrollo y Gestión de Software
+          </p>
+        </div>
+      </div>
+
+      <div className="text-primary text-2xl">
+        <button>Proyectos</button>
+        <button>Habilidades</button>
+        <button>Formación y experiencia</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
